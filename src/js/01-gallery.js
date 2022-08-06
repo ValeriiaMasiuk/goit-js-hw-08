3614f4efe50799cd69169ba53e6fe62a2b842187
Valeriia Masiuk <valmas1997@gmail.com>
2022-08-06 15:20:40 +0300

    adds task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-    // ** Create gallery of images
-
 const galleryBox = document.querySelector('.gallery');
 const galleryOfImages = createGallery(galleryItems);
+
+galleryBox.style.listStyle = 'none';
 
 galleryBox.insertAdjacentHTML('beforeend', galleryOfImages);
 
 // console.log(galleryBox)
 
 function createGallery(galleryItems) {
-    const gallery = galleryItems.map(({preview, original, description}) => {
+    const gallery = galleryItems.map(({ preview, original, description }) => {
         return `
-            <div class="gallery__item">
-                <a class="gallery__link" href="${preview}">
-                    <img
-                        class="gallery__image"
-                        src="${preview}"
-                        data-source="${original}"
-                        alt="${description}"
-                    />
-                </a>
-            </div>
+        <li>
+            <a class="gallery__item" href="${preview}">
+                <img class="gallery__image" src="${original}" alt="${description}" />
+            </a >
+        </li>
         `;
-    }).join('')
+    }).join('');
 
     return gallery;
 }
 
-
-// Добавь библиотеку SimpleLightbox как 
-// зависимость проекта используя
-// npm(ссылка на CDN из твоей прошлой работы больше не нужна).
-// Используй свой JavaScript код из предыдущей 
-// домашней работы, но выполни рефакторинг с учетом того,
-// что библиотека была установлена через npm(синтаксис import /export).
-// Для того чтобы подключить CSS код библиотеки в проект,
-// необходимо добавить еще один импорт,
-// кроме того который описан в документации.
-
-// // Описан в документации
-// import SimpleLightbox from "simplelightbox";
-// // Дополнительный импорт стилей
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
