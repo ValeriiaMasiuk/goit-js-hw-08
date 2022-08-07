e2eeefd010715e9227b52e5dabf3f3b8a5dcc36f
Valeriia Masiuk <valmas1997@gmail.com>
2022-08-08 00:32:15 +0300

    adds task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+
+const player = new Vimeo(iframe);
+// console.log(player)
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(currentTime, 1000))
+
+function currentTime({seconds}) {
+    localStorage.setItem(CURRENT_TIME, seconds)
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
 
