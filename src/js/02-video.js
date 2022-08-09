67cecebfeba898a5d3dea3608f36233baf35dd80
Valeriia Masiuk <valmas1997@gmail.com>
2022-08-10 01:20:45 +0300

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,24 +4,24 @@ import Vimeo from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 // console.log(iframe);
 
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 // console.log(player)
 
 const CURRENT_TIME = "videoplayer-current-time";
 
 player.on('timeupdate', throttle(currentTime, 1000))
 
-function currentTime(data) {
-    localStorage.setItem(CURRENT_TIME, data.seconds)
+let parsedTime = 0;
+
+function currentTime(evt) {
+    localStorage.setItem(CURRENT_TIME, evt.seconds)
 }
 
 const paused = localStorage.getItem(CURRENT_TIME);
 
-let parsedTime = 0;
-
 function timeSet(paused) {
     if (paused) {
-        parsedTime = JSON.parse(paused)
+        parsedTime = JSON.parse(paused);
     } else {
         parsedTime = 0;
     }
